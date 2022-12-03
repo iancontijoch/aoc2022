eb0debc2538a3fd14992c6bc66329cab66ee7f38
Ian Contijoch <ian.contijoch@gmail.com>
2022-12-03 11:04:35 -0500

    day 3

---
+++ day03/part1.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+from string import ascii_letters
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    priorities = 0
+    d = dict(zip(ascii_letters, range(1, 53)))
+    for line in lines:
+        mid = len(line) // 2
+        comp1, comp2 = line[:mid], line[mid:]
+        priorities += d[list(set(comp1) & set(comp2))[0]]
+    return priorities
+
+
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 1
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day03/part2.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+from string import ascii_letters
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    priorities = 0
+    d = dict(zip(ascii_letters, range(1, 53)))
+    num_groups = len(lines) // 3
+    groups = [tuple(lines[3*i:3*(i+1)]) for i in range(num_groups)]
+    for group in groups:
+        common_elem = list(set(group[0]) & set(group[1]) & set(group[2]))[0]
+        priorities += d[common_elem]
+    return priorities
+
+
+INPUT_S = '''\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+'''
+EXPECTED = 70
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
