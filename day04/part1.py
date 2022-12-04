d0fc5bd264d6b917a9baa6a57920298bdb2e2b8e
Ian Contijoch <ian.contijoch@gmail.com>
2022-12-04 10:54:34 -0500

    day 4

---
+++ day04/part1.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        left, right = line.split(',')
+        l1, l2 = map(int, left.split('-'))
+        r1, r2 = map(int, right.split('-'))
+
+        left_set, right_set = set(range(l1, l2+1)), set(range(r1, r2+1))
+        if left_set.issubset(right_set) or right_set.issubset(left_set):
+            total += 1
+
+    return total
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
